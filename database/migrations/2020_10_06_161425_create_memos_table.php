ff146c479c0da09078e927f1fa8edaea9200f38a
shioharugit <shiokawa_renraku@yahoo.co.jp>
2023-10-19 11:29:23 +0900

    メモ一覧UI改修のためのカラム変更

--- database/migrations/2020_10_06_161425_create_memos_table.php
+++ database/migrations/2020_10_06_161425_create_memos_table.php
@@ -18,7 +18,8 @@ class CreateMemosTable extends Migration
             $table->id();
             $table->bigInteger('user_id')->index()->comment('ユーザーID');
             $table->bigInteger('music_id')->index()->comment('曲ID');
-            $table->text('memo', 255)->nullable()->comment('メモ');
+            $table->text('memo')->nullable()->comment('メモ');
+            $table->tinyInteger('check_flag')->comment('メモ一覧画面でのフラグ');
             $table->timestamp('deleted_at')->nullable()->comment('削除日');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('登録日');
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('更新日');
