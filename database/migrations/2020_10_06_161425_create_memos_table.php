426a5065dad51b1cdd566d45b4bdfd45557813af
shioharu <shiokawa_renraku@yahoo.co.jp>
2024-10-10 08:39:51 +0900

    デフォルト値が欠如していたので修正

--- database/migrations/2020_10_06_161425_create_memos_table.php
+++ database/migrations/2020_10_06_161425_create_memos_table.php
@@ -19,7 +19,7 @@ class CreateMemosTable extends Migration
             $table->bigInteger('user_id')->index()->comment('ユーザーID');
             $table->bigInteger('music_id')->index()->comment('曲ID');
             $table->text('memo')->nullable()->comment('メモ');
-            $table->tinyInteger('check_flag')->comment('メモ一覧画面でのフラグ');
+            $table->tinyInteger('check_flag')->default(0)->comment('メモ一覧画面でのフラグ');
             $table->timestamp('deleted_at')->nullable()->comment('削除日');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'))->comment('登録日');
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'))->comment('更新日');
